73554d1e81ab0e58bc2a9030b58acad14c741f90
Alex <yexin@laiyifen.com>
2024-08-19 09:19:06 +0800

    Remove tracked log files

--- controllers/upload_file.go
+++ controllers/upload_file.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
-	"fmt"
+	"bytes"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"path/filepath"
-	"strings"
 
 	"github.com/Queen2333/ielts_test_backend/utils"
 	"github.com/gin-gonic/gin"
@@ -28,15 +29,69 @@ func UploadFile(c *gin.Context) {
 		return
 	}
 
-	// 定义上传文件的路径
-	filename := filepath.Base(file.Filename)
-	if err := c.SaveUploadedFile(file, "./uploads/"+filename); err != nil {
-		utils.HandleResponse(c, http.StatusInternalServerError, "", "upload file err: %s")
+	// 打开文件
+	src, err := file.Open()
+	if err != nil {
+		utils.HandleResponse(c, http.StatusInternalServerError, "", "Could not open file")
+		return
+	}
+	defer src.Close()
+
+	// 创建一个缓冲区和 multipart writer
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	// 创建一个 form-data field 来上传文件
+	fw, err := w.CreateFormFile("file", filepath.Base(file.Filename))
+	if err != nil {
+		utils.HandleResponse(c, http.StatusInternalServerError, "", "Could not create form file")
 		return
 	}
 
-	baseURL := fmt.Sprintf("%s%s", c.Request.URL.Scheme, c.Request.Host)
-	fileURL := strings.Join([]string{baseURL, "uploads", filename}, "/")
+	// 将文件内容复制到 multipart writer
+	if _, err = io.Copy(fw, src); err != nil {
+		utils.HandleResponse(c, http.StatusInternalServerError, "", "Could not copy file content")
+		return
+	}
+	w.Close()
 
-	utils.HandleResponse(c, http.StatusOK, fileURL, "Success")
+	// 创建一个 HTTP 请求并设置 multipart content type
+	flaskServerURL := "http://localhost:5001/upload" // Flask 服务器的 URL
+	req, err := http.NewRequest("POST", flaskServerURL, &b)
+	if err != nil {
+		utils.HandleResponse(c, http.StatusInternalServerError, "", "Could not create request")
+		return
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// 执行请求
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		utils.HandleResponse(c, http.StatusInternalServerError, "", "Could not send request to Flask server")
+		return
+	}
+	defer resp.Body.Close()
+
+	// 读取 Flask 服务器的响应
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		utils.HandleResponse(c, http.StatusInternalServerError, "", "Could not read Flask server response")
+		return
+	}
+
+	// 返回 Flask 服务器的响应
+	c.Data(resp.StatusCode, "application/json", body)
+
+	// // 定义上传文件的路径
+	// filename := filepath.Base(file.Filename)
+	// if err := c.SaveUploadedFile(file, "./uploads/"+filename); err != nil {
+	// 	utils.HandleResponse(c, http.StatusInternalServerError, "", "upload file err: %s")
+	// 	return
+	// }
+
+	// baseURL := fmt.Sprintf("%s%s", c.Request.URL.Scheme, c.Request.Host)
+	// fileURL := strings.Join([]string{baseURL, "uploads", filename}, "/")
+
+	// utils.HandleResponse(c, http.StatusOK, fileURL, "Success")
 }
